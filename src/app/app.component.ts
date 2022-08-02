8f00c77719da79c50593a0c4e4bf8298a17f63d9
AnitaAO <anitaisbeta@gmail.com>
2022-08-02 09:36:45 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>{{ title }}</h1>
+    <input (keyup.enter)="onKeyUp()">
+  `,
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+ title = "Event Filtering";
+
+ onKeyUp(){
+   console.log("Enter was pressed")
+ }
+}
